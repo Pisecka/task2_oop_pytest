ed7f2ccd479dc600eb4e83e09d7a8dec472e2916
Karina <pisecka.karina@gmail.com>
2022-01-15 15:39:14 +0200

    update method add_area

--- src/Rectangle.py
+++ src/Rectangle.py
@@ -1,5 +1,5 @@
 from src.Figure import Figure
-
+from src.Circle import Circle
 
 class Rectangle(Figure):
 
@@ -15,4 +15,7 @@ class Rectangle(Figure):
         return self.a * 2 + self.b  * 2
 
 
+
+
 r1 = Rectangle('rect1', 5, 10)
+c1 = Circle('circle1', 10)
